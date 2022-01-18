409a8ad3ee4eef4ded55ad1f90d289bf408f9b54
muhaimenul <muhaimenulislam@iut-dhaka.edu>
2022-01-18 14:29:23 +0600

    test loading compoenent

--- src/lib/components/Loader.js
+++ src/lib/components/Loader.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./Loading.module.css";
 
-const Loading = (props) => {
+const Loading = ({ loading = true}) => {
     return (
-        <div className={props.loading ? styles.loading : styles.none}>
+        <div className={loading ? styles.loading : styles.none}>
             <div className={styles.uil_ring_css} style={{ transform: 'scale(0.79)' }}>
                 <div></div>
             </div>
